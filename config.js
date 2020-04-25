7ceee91a463f5173c344869211e8c6169c861a38
Danilo Pedrosa <danilomopedrosa@gmail.com>
2020-04-24 23:57:51 -0300

    add blog

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by Danilo Pedrosa',
+  subtitle: 'Software Engineer at Loggi.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Danilo Pedrosa',
+    photo: '/photo.jpeg',
+    bio: 'Software Engineer',
+    contacts: {
+      email: 'danilomopedrosa@gmail.com',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: 'dmop',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'danilo-pedrosa-21174b115/',
+      instagram: 'pedrosaadanilo',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
